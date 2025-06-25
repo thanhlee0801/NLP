7abca2cb3f2841054eda987cb6f5bd5a8a2c881d
Lê Duy Thành <152592360+thanhlee0801@users.noreply.github.com>
2025-06-25 17:12:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import load_model
+
 import joblib
 import os # Import os để kiểm tra sự tồn tại của file
 
